2f1dde1f0d7a91c863c507760d2f6bed19874bc1
Yariv Garala <yariv1052@gmail.com>
2021-06-17 02:55:11 +0300

    Fixing and refactoring of methods

--- src/components/TicTacToeBoard.vue
+++ src/components/TicTacToeBoard.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="tic-tac-toe-board" id="board">
+
     <div :key="i.value" v-for="(n, i) in 3">
       <div :key="j.value" v-for="(n, j) in 3">
 
         <cell @click="play(i, j)"
               :value="board[i][j]">
         </cell>
-
       </div>
     </div>
+
   </div>
 </template>
 
@@ -25,9 +26,9 @@
     data() {
       return {
         board: [
-          ['', '', ''],
-          ['', '', ''],
-          ['', '', '']
+          Array(3).fill(null),
+          Array(3).fill(null),
+          Array(3).fill(null),
         ],
         inProgress: true,
         stepNumber: 0,
@@ -44,32 +45,29 @@
       // Play
       play(x, y) {
         // Game rounds:
-        // 1. check's move
-        // 2. place symbol
+        // 1. check's move + place symbol + switch turns
         // 2. check for win or tie
-        // 4. switch turns
-        // 5. restart
+        // 3. update winner / tie
 
         this.placeSymbol(x, y)
         this.hasWinner()
 
-        if ((this.winner === 'x' || this.winner === 'o') && !this.inProgress) {
-          console.log(`${this.winner} IS THE WINNER!`)
+        if (this.winner !== null && !this.inProgress) {
+          this.updateScore()
           this.$emit('Has winner') // Game ended
-
-        } else if (this.winner === null && this.stepNumber === 9 && !this.inProgress) {
-          console.log('Tie')
-          this.$emit('HasTie') // Game ended
+          return
         }
-
-        this.swapTurns()
-
+        else if (this.winner === null && this.stepNumber === 9) {
+          this.updateScore()
+          this.$emit('HasTie') // Game ended
+          return
+        }
       },
 
       // Places the symbol on the board
       placeSymbol(x, y) {
 
-        if (this.board[x][y] !== '') {
+        if (this.board[x][y] !== null) {
           // Invalid move - No action will be taken
           console.log("Invalid move")
           return
@@ -78,67 +76,45 @@
         this.board[x][y] = this.currentPlayer['player']
         this.stepNumber += 1
         this.$forceUpdate()
+        this.swapTurns()
       },
 
       // Checks for winner
       hasWinner(x, y) {
 
-        // for (let i = 0; i < 3; i++) {
-        //   for (let j = 0; j < 1; j++) {
-        //
-        //     // checks row
-        //     if (this.board[i][j] !== '' && this.board[i][j] === this.board[i][j+1] ===
-        //       this.board[i][j+2]) {
-        //       this.winner = this.board[i][j]
-        //       this.inProgress = false
-        //       return
-        //     }
-        //     // checks column
-        //     if (this.board[j][i] !== '' && this.board[j][i] ===
-        //       this.board[j][i+1] === this.board[j][i+2]) {
-        //       this.winner = this.board[j][i]
-        //       this.inProgress = false
-        //       return
-        //     }
-        //     // TODO: checks diagonal
-        //     if (this.board[i][i] !== '' && this.board[i][i] ===
-        //       this.board[i+1][i+1] === this.board[i+2][i+2]) {
-        //       this.winner = this.board[i][i]
-        //       this.inProgress = false
-        //       return
-        //     }
-        //     if(this.board[j][j+2] !== '' && this.board[i][j+2] ===
-        //       this.board[i+1][j+1] === this.board[i+2][j]) {
-        //       this.winner = this.board[i][i]
-        //       return
-        //     }
-        //   }
-        // }
         const winningCombination = [
-          [0, 1, 2],
-          [3, 4, 5],
-          [6, 7, 8],
-          [0, 3, 6],
-          [1, 4, 7],
-          [2, 5, 8],
-          [0, 4, 8],
-          [2, 4, 6],
+          [[0, 0], [0, 1], [0, 2]],
+          [[1, 0], [1, 1], [1, 2]],
+          [[2, 0], [2, 1], [2, 2]],
+          [[0, 0], [1, 0], [2, 0]],
+          [[0, 1], [1, 1], [2, 1]],
+          [[0, 2], [1, 2], [2, 2]],
+          [[0, 0], [1, 1], [2, 2]],
+          [[0, 2], [1, 1], [2, 0]],
         ]
-        // For each winning combination - search for a winner
-        // winningCombination.forEach((position) => {
-        //   const [a, b, c] = position
-        //   if (this.board[a][a] && this.board[a][a] === this.board[a][b] &&
-        //     this.board[a][a] === this.board[a][c]) {
-        //     this.inProgress = false
-        //     console.log("The winner is:", this.winner)
-        //     return this.board[a][a] //  'O' or 'X'
-        //   }
-        // })
 
-          // If there's winner
+        // For each winning combination - search for a winner
+        const res = winningCombination.forEach((position) => {
+          const [[a0, a1], [b0, b1], [c0, c1]] = position
+
+          if (this.board[a0][a1] && this.board[a0][a1] === this.board[b0][b1] &&
+            this.board[a0][a1] === this.board[c0][c1]) {
+            // console.log("The winner is:", this.board[a0][a1])
+            this.inProgress = false
+            this.winner = this.board[a0][a1]
+            return true
+          }
+          return false
+        })
+        return res
+      },
+
+      // Update score board
+      updateScore(){
+        // If there's winner
         this.$store.commit({
-          type: 'updateWinnerAndScore',
-          player: this.currentPlayer
+          type: 'updateScoreBoard',
+          player: this.winner
         })
       },
 
@@ -149,23 +125,23 @@
           playing: player
         })
       },
+
     },
 
     // Clear our board
     watch: {
       ['reset']() {
         // code that's clear the game board
-        for(let i = 0; i < 3; i++){
-          for(let j = 0; j < 3; j++){
-            this.board[i][j] = ''
-          }
-        }
-
-        this.item.find((item) => {
-          item
-        })
-        // this.$emit('boardIsCleared')
-        console.log("Watcher reset")
+        this.board = [
+          Array(3).fill(null),
+          Array(3).fill(null),
+          Array(3).fill(null)
+        ]
+        this.inProgress = true
+        this.stepNumber = 0
+        this.value = ''
+        this.winner = null
+        this.$emit('boardIsCleared')
       }
     },
 
