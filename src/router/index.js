9f4d08a326a9d413be6914695058a467d745a7f8
Yariv Garala <yariv1052@gmail.com>
2021-06-17 10:50:16 +0300

    Updating routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import registration from '@/components/Registration'
-import registrations from '@/components/Registrations'
 import TicTacToeGame from '@/components/TicTacToeGame'
 
 
@@ -13,16 +11,6 @@ export default new Router({
       path: '/',
       name: 'TicTacToeGame',
       component: TicTacToeGame
-    },
-    {
-      path: '/registration',
-      name: 'Registration',
-      component: registration
-    },
-    {
-      path: '/registrations',
-      name: 'Registrations',
-      component: registrations
     },
   ]
 })
